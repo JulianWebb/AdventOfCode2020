2d208be16f473b46fc348a9c1ef8256f4feeb20b
Julian Webb <git@julianwebb.ca>
2020-12-02 03:26:07 -0800

    Moved const out of comparison

--- 1/b/main.js
+++ 1/b/main.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 
+const comparisonValue = 2020;
+
 fs.readFile('expense-report', { encoding: 'utf8' }, (error, data) => {
-    if (error) throw error;
+    if (error) return console.log(error);
     let entries = data.split(`\n`);
     
     entries.forEach(firstEntry => {
         entries.forEach(secondEntry => {
             entries.forEach(thirdEntry => {
                 let entrySum = parseInt(firstEntry) + parseInt(secondEntry) + parseInt(thirdEntry);
-                if (entrySum == 2020) {
+                if (entrySum == comparisonValue) {
                     let entryProduct = firstEntry * secondEntry * thirdEntry;
                     console.log(entryProduct);
                     process.exit(0);
