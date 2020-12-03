fa1084080870a5fadd5b94c5da99ec502e704d89
Julian Webb <git@julianwebb.ca>
2020-12-03 06:23:45 -0800

    Day Three: Second Part

---
+++ 3/b/main.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const { start } = require('repl');
+
+const Map = require('./map');
+const offsets = require('./offsets.json');
+
+let simulateTravel = async (map, startingCoordinates, offsets) => {
+    let sled = {
+        x: startingCoordinates[0],
+        y: startingCoordinates[1]
+    }
+
+    let treeCount = 0;
+    do {
+        treeCount += map.checkSegement(sled.x, sled.y);
+        sled.x += offsets[0];
+        sled.y += offsets[1]
+    } while (sled.y < map.height);
+
+    return treeCount;
+}
+
+fs.readFile('slope-map', { encoding: 'utf8' }, (error, data) => {
+    if (error) throw error;
+
+    let map = new Map(data.split('\n'));
+    
+    let simulations = [];
+    offsets.forEach(offset => {
+        simulations.push(simulateTravel(map, [0, 0], [offset.x, offset.y]))
+    })
+
+    Promise.all(simulations).then(results => {
+        let trees = results.reduce((previous, current) => previous * current, 1);
+        console.log(`Trees: ${trees}`);
+        process.exit(0);
+    })
+})
